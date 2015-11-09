b25c3f7918654e010ea65470e957d8712a881768
Anatoliy Kmetyuk <anatoliykmetyuk@gmail.com>
2015-11-09 23:32:49 +0200

    DummyRepository removed

--- src/main/scala/eyetest/Main.scala
+++ src/main/scala/eyetest/Main.scala
@@ -29,19 +29,3 @@ object Main extends SubScriptApplication {
                                      let repos = new DerbyRepo("eyetest-db")
 
 }
-
-object DummyRepositories extends Repositories {
-
-  def user = new UserRepo {script..
-    all = success: Seq("Jack", "John", "Ann")
-    write(username: String) = println("Registered " + username)
-  }
-
-  def score = new ScoreRepo {script..
-    scoresOf(user: String): Any = success(for (i <- 1 to 10) yield (new Date, 15 - i / 10D, 16 - i / 10D))
-    write(user: String, score: (Double, Double)) = println("Writing for " + user + " with score " + score)
-  }
-
-  override def close = ()
-
-}
