b9e41b87454350031d1c4ea9841a73051dde3803
Anatoliy Kmetyuk <anatoliykmetyuk@gmail.com>
2015-11-09 20:17:48 +0200

    Results have larger font

--- src/main/scala/eyetest/app/Result.scala
+++ src/main/scala/eyetest/app/Result.scala
@@ -3,7 +3,7 @@ package eyetest.app
 import subscript.language
 import scala.language.implicitConversions
 
-import java.awt.Point
+import java.awt.{Point, Font}
 
 import scala.swing._
 import scala.swing.BorderPanel.Position._
@@ -18,9 +18,10 @@ class Result(rightEye: Double, leftEye: Double) extends Frame with FrameProcess 
   title = "Result"
   location = new Point(300, 300)
 
+  val labelFont = new Font("Ariel", Font.PLAIN, 30)
 
-  val rightEyeLbl = new Label(s"Right eye: $rightEye")
-  val leftEyeLbl  = new Label(s"Left eye : $leftEye")
+  val rightEyeLbl = new Label(s"Right eye: $rightEye") {font = labelFont}
+  val leftEyeLbl  = new Label(s"Left eye : $leftEye" ) {font = labelFont}
 
   val mainPanel = new GridPanel(2, 1) {
     contents += rightEyeLbl
